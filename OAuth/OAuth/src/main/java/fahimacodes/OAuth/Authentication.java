153d4fd876911433ba9eed5da39e268a7a3b01eb
Fahima <78881936+Fahimacodes@users.noreply.github.com>
2021-04-14 16:55:39 +0100

    Update Authentication.java

--- OAuth/OAuth/src/main/java/fahimacodes/OAuth/Authentication.java
+++ OAuth/OAuth/src/main/java/fahimacodes/OAuth/Authentication.java
@@ -96,39 +96,4 @@ public class Authentication {
 
 		return oAuth;
 	}
-
-	/**
-	* Container class for OAuth 2.0 to hold authentication & authorization token 
-	 * of users to make subsequent Zoom API requests
-	 */
-	public class OAuth {
-
-		private String accessToken;
-		private String refreshToken;
-
-		public OAuth() {
-		}
-
-		public OAuth(String accessToken, String refreshToken) {
-
-			this.setAccessToken(accessToken);
-			this.setRefreshToken(refreshToken);
-		}
-
-		public String getAccessToken() {
-			return accessToken;
-		}
-
-		public void setAccessToken(String accessToken) {
-			this.accessToken = accessToken;
-		}
-
-		public String getRefreshToken() {
-			return refreshToken;
-		}
-
-		public void setRefreshToken(String refreshToken) {
-			this.refreshToken = refreshToken;
-		}
-	}
 }
